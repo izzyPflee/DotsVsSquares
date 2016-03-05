77bdfc41f66d9b0568ac7af8017a4e36607032ae
eody1 <eody1@hotmail.com>
2016-03-04 16:48:44 -0800

    added new classes for key events and the jframe on the client side

---
+++ src/Client/ClientKeyEventHandler.java
@@ -0,0 +1,49 @@
+package Client;
+
+import java.awt.event.KeyEvent;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class ClientKeyEventHandler {
+	
+	private Socket socket;
+	private PrintWriter out;
+	
+	public ClientKeyEventHandler(Socket socket)
+	{
+		this.socket = socket;
+		//out = new PrintWriter(socket.getInputStream());
+	}
+	
+	public void processKey(KeyEvent input)
+	{
+		int key = input.getKeyCode();
+		
+		switch(key)
+		{
+		case KeyEvent.VK_UP:{} //move up
+
+		case KeyEvent.VK_W:{} //move up
+
+		case KeyEvent.VK_DOWN:{} //move down
+
+		case KeyEvent.VK_S:{} //move down
+
+		case KeyEvent.VK_LEFT:{} //move left
+
+		case KeyEvent.VK_A:{} //move left
+
+		case KeyEvent.VK_RIGHT:{} //move right
+
+		case KeyEvent.VK_D:{} //move right
+
+		case KeyEvent.VK_Q: // client quitting disconnect 
+		{
+			System.out.println("THIS SHOULD CRASH NOW -- we need to add a listener on the server side");
+			out.println(key);
+		}
+
+		}			
+	}
+}
--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,5 +1,11 @@
 package Client;
 
+/*
+ * clientMain connects to the server, creates the other threads and sends move requests to the server
+ */
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,57 +14,86 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import Renderer.GameShape;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ClientMain {
+import Renderer.GameShape;
+import Renderer.Renderer;
+
+public class ClientMain extends JFrame implements KeyListener {
+
+	private String serverAddress = "localhost"; //This needs to be changed to the servers hard coded address
+	private int portNumber = 9898;
+	private Socket socket;
+	private ArrayList<GameShape> gameShapeArray;
+	private ClientKeyEventHandler commandForServer;
+	private Renderer renderer;
 
 
 	public static void main(String[] args)
 	{
-		String serverAddress = "localhost"; //This needs to be changed to the servers hard coded address
-		int portNumber = 9898;
-		Socket socket;
-		ArrayList<GameShape> gameShapeArray;
+		new ClientMain();
+	}
+
+	public ClientMain()
+	{
+
 
 		try 
 		{
+			
 			socket = new Socket(serverAddress, portNumber);
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			String startGame = in.readLine();
-
-			if(startGame.compareTo("NO ROOM") == 0) //if Server is full or unable to accept players it 
-			{									//will respond with "NO ROOM"
-				System.out.println("No Room On Server Ending game");
-				return;
-			}
+			//PrintWriter out =  new PrintWriter(socket.getOutputStream(), true);
+			//out.println("join");
+			//BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			
-			
+			//System.out.println("Here in client");
+			//String startGame = in.readLine();
+			//System.out.println(startGame);
+
+			//if(startGame.compareTo("NO ROOM") == 0) //if Server is full or unable to accept players it 
+			//{									//will respond with "NO ROOM"
+			//	System.out.println("No Room On Server Ending game");
+			//	return;
+			//}
+	
+
 			//======================================After connection with server is successful============
-
+			
+	
 			gameShapeArray = new ArrayList<GameShape>();
-			PrintWriter out = new PrintWriter(socket.getOutputStream());
 			
-			
-			
+
+			renderer = new Renderer(800, 800, this);
+			this.addKeyListener(this);
+			commandForServer = new ClientKeyEventHandler(socket);
 			
 
 		} 
-		catch (IOException e) 
+		catch (Exception e) 
 		{
 			System.out.println("There Was An ERROR and the Game CANNOT run now.");
 			e.printStackTrace();
 		}
 
 
+	}
 
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		//send the command to the server
+		commandForServer.processKey(arg0);
+	}
 
-		
+	@Override
+	public void keyReleased(KeyEvent arg0) { //unused
+		// TODO Auto-generated method stub
 
+	}
 
-
-
+	@Override
+	public void keyTyped(KeyEvent arg0) { //unused
+		// TODO Auto-generated method stub
 
 	}
 
